839d4960cf97b6b3260a22e912240981a9ded4f8
harveerd123 <harveerdole@gmail.com>
2025-06-04 22:08:49 +0100

    Added all pages content for application

--- HarveerPortfolio/Program.cs
+++ HarveerPortfolio/Program.cs
@@ -1,4 +1,5 @@
 using HarveerPortfolio;
+using HarveerPortfolio.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,14 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<GitHubService>();
+
+builder.Services.AddHttpClient<GitHubService>(client =>
+{
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("request");
+});
+
+builder.Services.AddScoped(sp => new HttpClient());
 
 await builder.Build().RunAsync();
